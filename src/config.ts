a456dcf9aeb5910427346c8c9a127bb9ac68a279
techlead <belabesmohammed@gmail.com>
2019-04-09 22:59:58 +0200

    checks

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import * as storage from '@google-cloud/storage'
 admin.initializeApp()
 
 // Firestore
-export const db = admin.firestore()
+export const db: FirebaseFirestore.Firestore = admin.firestore()
 db.settings({ timestampsInSnapshots: true })
 export const timestamp: FirebaseFirestore.FieldValue = admin.firestore.FieldValue.serverTimestamp()
 export const ordersRef: FirebaseFirestore.CollectionReference = db.collection(
@@ -12,9 +12,9 @@ export const ordersRef: FirebaseFirestore.CollectionReference = db.collection(
 )
 
 // Storage
-export const gcs = new storage.Storage()
-export const bucketName = functions.config().bucket.name
-export const bucket = gcs.bucket(bucketName)
+export const gcs: storage.Storage = new storage.Storage()
+export const bucketName: string = functions.config().bucket.name
+export const bucket: storage.Bucket = gcs.bucket(bucketName)
 export const signedUrlCfg: {
   action: 'read' | 'write' | 'delete' | 'resumable'
   expires: string
@@ -25,7 +25,7 @@ export const overlayURL: string =
   'https://i.kym-cdn.com/photos/images/masonry/001/452/786/271.png'
 const size: string = 'auto'
 const encoding: any = null
-export const key = functions.config().removebg.key
+export const key: string = functions.config().removebg.key
 export const apiOpts = {
   method: 'POST',
   uri: 'https://api.remove.bg/v1.0/removebg',
